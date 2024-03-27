481c4ec8d1c39de5c7b7e68823dd056a29d73b01
Haifa Gaza <haifalapuz@gmail.com>
2024-03-27 14:18:55 +0900

    Simplify expressions

--- chronoweb/src/main/java/org/dfpl/chronograph/kairos/program/path_reachability/OutIsAfterPathReachability.java
+++ chronoweb/src/main/java/org/dfpl/chronograph/kairos/program/path_reachability/OutIsAfterPathReachability.java
@@ -1,6 +1,7 @@
 package org.dfpl.chronograph.kairos.program.path_reachability;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.Set;
 import java.util.function.BiPredicate;
 import java.util.function.Predicate;
@@ -22,123 +23,108 @@ import com.tinkerpop.blueprints.Vertex;
 
 public class OutIsAfterPathReachability extends AbstractKairosProgram<Document> {
 
-	public OutIsAfterPathReachability(Graph graph, GammaTable<String, Document> gammaTable) {
-		super(graph, gammaTable, "OutIsAfterPathReachability");
-	}
+    public OutIsAfterPathReachability(Graph graph, GammaTable<String, Document> gammaTable) {
+        super(graph, gammaTable, "OutIsAfterPathReachability");
+    }
 
-	Predicate<Document> sourceTest = new Predicate<Document>() {
-		@Override
-		public boolean test(Document t) {
-			if (t == null)
-				return false;
-			return true;
-		}
-	};
+    private static Predicate<Document> IS_SOURCE_VALID = Objects::nonNull;
 
-	BiPredicate<Document, Document> targetTest = new BiPredicate<Document, Document>() {
-		@Override
-		public boolean test(Document t, Document u) {
-			if (u.getLong("time") < t.getLong("time"))
-				return true;
+    private static BiPredicate<Document, Document> IS_AFTER = (t, u) -> u.getLong("time") < t.getLong("time");
 
-			return false;
-		}
-	};
+    @Override
+    public void onInitialization(Set<Vertex> sources, Long startTime, String edgeLabel) {
+        this.edgeLabel = edgeLabel;
+        synchronized (gammaTable) {
+            for (Vertex s : sources) {
+                String id = s.getId();
+                gammaTable.set(id, id, new PathGammaElement(List.of(id), startTime));
+            }
 
-	@Override
-	public void onInitialization(Set<Vertex> sources, Long startTime, String edgeLabel) {
-		this.edgeLabel = edgeLabel;
-		synchronized (gammaTable) {
-			for (Vertex s : sources) {
-				String id = s.getId();
-				gammaTable.set(id, id, new PathGammaElement(List.of(id), startTime));
-			}
+            if (graph instanceof MChronoGraph mg) {
+                mg.getEdgeEvents().forEach(event -> {
+                    System.out.println("\t\t" + event);
+                    String out = event.getVertex(Direction.OUT).getId();
+                    String in = event.getVertex(Direction.IN).getId();
+                    gammaTable.append(sources.parallelStream().map(v -> v.getId()).collect(Collectors.toSet()), out,
+                            IS_SOURCE_VALID, in, new PathGammaElement(List.of(in), event.getTime()), IS_AFTER);
+                    gammaTable.print();
+                });
+            } else if (graph instanceof PChronoGraph pg) {
+                pg.getEdgeEvents().forEach(event -> {
+                    System.out.println("\t\t" + event);
+                    String out = event.getVertex(Direction.OUT).getId();
+                    String in = event.getVertex(Direction.IN).getId();
+                    gammaTable.append(sources.parallelStream().map(v -> v.getId()).collect(Collectors.toSet()), out,
+                            IS_SOURCE_VALID, in, new PathGammaElement(List.of(in), event.getTime()), IS_AFTER);
+                    gammaTable.print();
+                });
+            }
+        }
+    }
 
-			if (graph instanceof MChronoGraph mg) {
-				mg.getEdgeEvents().forEach(event -> {
-					System.out.println("\t\t" + event);
-					String out = event.getVertex(Direction.OUT).getId();
-					String in = event.getVertex(Direction.IN).getId();
-					gammaTable.append(sources.parallelStream().map(v -> v.getId()).collect(Collectors.toSet()), out,
-							sourceTest, in, new PathGammaElement(List.of(in), event.getTime()), targetTest);
-					gammaTable.print();
-				});
-			} else if (graph instanceof PChronoGraph pg) {
-				pg.getEdgeEvents().forEach(event -> {
-					System.out.println("\t\t" + event);
-					String out = event.getVertex(Direction.OUT).getId();
-					String in = event.getVertex(Direction.IN).getId();
-					gammaTable.append(sources.parallelStream().map(v -> v.getId()).collect(Collectors.toSet()), out,
-							sourceTest, in, new PathGammaElement(List.of(in), event.getTime()), targetTest);
-					gammaTable.print();
-				});
-			}
-		}
-	}
+    @Override
+    public void onAddEdgeEvent(EdgeEvent addedEvent) {
+        synchronized (gammaTable) {
+            String out = addedEvent.getVertex(Direction.OUT).getId();
+            String in = addedEvent.getVertex(Direction.IN).getId();
+            gammaTable.update(out, IS_SOURCE_VALID, in, new PathGammaElement(List.of(in), addedEvent.getTime()), IS_AFTER);
+            gammaTable.print();
+        }
+    }
 
-	@Override
-	public void onAddEdgeEvent(EdgeEvent addedEvent) {
-		synchronized (gammaTable) {
-			String out = addedEvent.getVertex(Direction.OUT).getId();
-			String in = addedEvent.getVertex(Direction.IN).getId();
-			gammaTable.update(out, sourceTest, in, new PathGammaElement(List.of(in), addedEvent.getTime()), targetTest);
-			gammaTable.print();
-		}
-	}
+    @Override
+    public void onRemoveEdgeEvent(EdgeEvent removedEdge) {
 
-	@Override
-	public void onRemoveEdgeEvent(EdgeEvent removedEdge) {
+    }
 
-	}
+    @Override
+    public void onAddVertex(Vertex addedVertex) {
 
-	@Override
-	public void onAddVertex(Vertex addedVertex) {
+    }
 
-	}
+    @Override
+    public void onAddEdge(Edge addedEdge) {
 
-	@Override
-	public void onAddEdge(Edge addedEdge) {
+    }
 
-	}
+    @Override
+    public void onUpdateVertexProperty(Document previous, Document updated) {
 
-	@Override
-	public void onUpdateVertexProperty(Document previous, Document updated) {
+    }
 
-	}
+    @Override
+    public void onUpdateEdgeProperty(Document previous, Document updated) {
 
-	@Override
-	public void onUpdateEdgeProperty(Document previous, Document updated) {
+    }
 
-	}
+    @Override
+    public void onRemoveVertex(Vertex removedVertex) {
 
-	@Override
-	public void onRemoveVertex(Vertex removedVertex) {
+    }
 
-	}
+    @Override
+    public void onRemoveEdge(Edge removedEdge) {
 
-	@Override
-	public void onRemoveEdge(Edge removedEdge) {
+    }
 
-	}
+    @Override
+    public void onAddVertexEvent(VertexEvent addedVertexEvent) {
 
-	@Override
-	public void onAddVertexEvent(VertexEvent addedVertexEvent) {
+    }
 
-	}
+    @Override
+    public void onUpdateVertexEventProperty(Document previous, Document updated) {
 
-	@Override
-	public void onUpdateVertexEventProperty(Document previous, Document updated) {
+    }
 
-	}
+    @Override
+    public void onUpdateEdgeEventProperty(Document previous, Document updated) {
 
-	@Override
-	public void onUpdateEdgeEventProperty(Document previous, Document updated) {
+    }
 
-	}
+    @Override
+    public void onRemoveVertexEvent(VertexEvent removedVertex) {
 
-	@Override
-	public void onRemoveVertexEvent(VertexEvent removedVertex) {
-
-	}
+    }
 
 }
